ea41f7dd7a55c31f53a64f6e3c30e5d1e1eff296
chaoge <2502247952@qq.com>
2022-08-28 02:43:14 +0800

    bb

--- bilibili/views.py
+++ bilibili/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-import os
+import shutil
 
 
 def index(request):
@@ -8,24 +8,35 @@ def index(request):
 
 
 def down(request):
-    url=request.POST.get("url")
+    url = request.POST.get("url")
     if url == "":
         return HttpResponse("hello youtubedl")
     else:
         down_video(url)
         return HttpResponse("down successful")
 
+
 from yt_dlp import YoutubeDL
+
+
 def down_video(url):
     # path
-    #path = "/home/chaoge/test/"
+    path = "/home/chaoge/test/"
     # cloudpath
-    #path = "/home/data/"
-    URLS = [url]
+    # path = "/home/data/"
+
+    #URLS = [url]
 
     ydl_opts = {
-        'noplaylist': True,
-        "outtmpl": path + '%(title)s.%(ext)s'
+        'outtmpl': path + '%(title)s.%(ext)s',
+        'noplaylist': True
     }
     with YoutubeDL(ydl_opts) as ydl:
-        ydl.download(URLS)
+        result = ydl.extract_info(
+            url,  # 视频链接
+            download=True,  # 不下载只是抽取信息
+        )
+    title = result["title"]
+    old = "/home/data/_.mp4"
+    new = "/home/data/%s.mp4" % title
+    shutil.move(old, new)
